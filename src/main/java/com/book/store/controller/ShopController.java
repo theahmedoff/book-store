810ac8433e33f916ebc78219b08c0167b433e2e2
Ahmedov Aslan Karim <Ahme_vn75@itstep.edu.az>
2019-02-02 12:31:15 +0400

    Page shop edited

--- src/main/java/com/book/store/controller/ShopController.java
+++ src/main/java/com/book/store/controller/ShopController.java
@@ -2,10 +2,15 @@ package com.book.store.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/shop")
 public class ShopController {
+
+    @RequestMapping("/shop")
+    public String getBiographyBooks(@RequestParam("{id-category}")int idCategory){
+        return "view/shop";
+    }
 
 
 
