11bceb5dc76220ce2acb52a3df32a6370b9ba4a0
Ahmedov Aslan Karim <Ahme_vn75@itstep.edu.az>
2019-02-03 11:10:41 +0400

    book repository added

---
+++ src/main/java/com/book/store/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.book.store.repository;
+
+import com.book.store.model.Book;
+
+import java.util.List;
+
+public interface BookRepository {
+    public List<Book> getAllBooks();
+    public List<Book> getBooksByIdCategory(int id);
+    public List<Book> getLastBooks();
+}
---
+++ src/main/java/com/book/store/repository/BookRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.book.store.repository;
+
+import com.book.store.model.Book;
+
+import java.util.List;
+
+public class BookRepositoryImpl implements BookRepository{
+    @Override
+    public List<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooksByIdCategory(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getLastBooks() {
+        return null;
+    }
+}
