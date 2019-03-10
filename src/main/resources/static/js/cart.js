28435d2baf53b163cbc73a3d9cccd54d7ede43c9
Ahmedov Aslan Karim <Ahme_vn75@itstep.edu.az>
2019-03-10 13:57:25 +0400

    cart.js update

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,3 +1,6 @@
+var id;
+var quantityArr = [];
+
 $(function () {
     var pageName = $('#pageName').val();
     if (pageName === 'cartPage') {
@@ -14,17 +17,26 @@ function getCarts(){
         success: function (carts) {
             $('#idCarts').empty();
             carts.forEach(function (cart) {
+                $('#count').clear;
                 $('#idCarts').append('<tr>')
                     $('#idCarts').append('<td class="product-thumbnail"><a href="' + "/single-product?idBook=" + cart.book.idBook + '"><img src="/images/product/sm-3/1.jpg" alt=""></a></td>');
                     $('#idCarts').append('<td class="product-name"><a href="' + "/single-product?idBook=" + cart.book.idBook + '">' + cart.book.title + '</a></td>')
                     $('#idCarts').append('<td class="product-price"><span class="amount">' + "$" + cart.book.stock.price + '</span></td>');
-                    $('#idCarts').append('<td class="product-stock-status"><input type="number" value="'+ cart.quantity + '">'+'</input></td>');
+                    $('#idCarts').append('<td class="product-stock-status"><input type="number" id="count" value="' + cart.quantity + '" class="classCartQuantity"/></td>');
                     $('#idCarts').append('<td class="product-subtotal">' + "$" + cart.subTotals + '</td>');
-                    $('#idCarts').append('<td class="product-remove"> <button type="button" onclick="deleteCartById('+ cart.idCart +')" >X</button></td>')
+                $('#idCarts').append('<td class="product-remove"> <button class="zmdi zmdi-delete" type="button" onclick="deleteCartById('+ cart.idCart +')" ></button></td>');
+                $('#idCarts').append('<input type="hidden" value="'+cart.idCart+'" class="classIdCart"/>');
                 $('#idCarts').append('</tr>')
+
+
+
             })
         }
     })
+}
+
+function getQuant() {
+    console.log(quantityArr);
 }
 
 function getMinicarts() {
@@ -33,8 +45,8 @@ function getMinicarts() {
         url: "/cart/get-carts",
         data: "JSON",
         success: function (carts) {
-            console.log(carts);
-            $('#startShping').append('<div class="minicart-content-wrapper">\n' +
+            $('#startShoping').empty();
+            $('#startShoping').append('<div class="minicart-content-wrapper">\n' +
                 '                                    <div class="micart__close">\n' +
                 '                                        <span>close</span>\n' +
                 '                                    </div>\n' +
@@ -46,7 +58,7 @@ function getMinicarts() {
                 '                                        <span id="idCardSubtotal">0</span>\n' +
                 '                                    </div>\n' +
                 '                                    <div class="mini_action checkout">\n' +
-                '                                        <a class="checkout__btn" href="cart.html">Go to Checkout</a>\n' +
+                '                                        <a class="checkout__btn" href="/cart">Go to Checkout</a>\n' +
                 '                                    </div>\n' +
                 '                                    <div class="single__items">\n' +
                 '                                        <div class="miniproduct" id="minicart">\n' +
@@ -66,7 +78,7 @@ function getMinicarts() {
                     '                                                            alt="product images"></a>\n' +
                     '                                                </div>\n' +
                     '                                                <div class="content">\n' +
-                    '                                                    <h6><a href="product-details.html">cart.book.title</a></h6>\n' +
+                    '                                                    <h6><a href="product-details.html">' + cart.book.title + '</a></h6>\n' +
                     '                                                    <span class="prize">$'+ cart.book.stock.price +'</span>\n' +
                     '                                                    <div class="product_prize d-flex justify-content-between">\n' +
                     '                                                        <span class="qun">Qty: '+ cart.quantity +'</span>\n' +
@@ -83,45 +95,6 @@ function getMinicarts() {
 
 
             $('#startShping').append('</div>');
-        },
-        error: function () {
-            alert("Error mini carts")
-        }
-    })
-}
-function getMinicarts2() {
-    $.ajax({
-        type: "GET",
-        url: "/cart/get-carts",
-        data: "JSON",
-        success: function (carts) {
-
-            $('#startShping').empty();
-            $('#startShping').append('<div class="block-minicart minicart__active" id="startShping">\n' +
-                '                                <div class="minicart-content-wrapper">\n' +
-                '                                    <div class="micart__close">\n' +
-                '                                        <span>close</span>\n' +
-                '                                    </div>\n' +
-                '                                    <div class="items-total d-flex justify-content-between">\n' +
-                '                                        <span>3 items</span>\n' +
-                '                                        <span>Cart Subtotal</span>\n' +
-                '                                    </div>\n' +
-                '                                    <div class="total_amount text-right">\n' +
-                '                                        <span>$66.00</span>\n' +
-                '                                    </div>\n' +
-                '                                    <div class="mini_action checkout">\n' +
-                '                                        <a class="checkout__btn" href="cart.html">Go to Checkout</a>\n' +
-                '                                    </div>\n' +
-                '                                    <div class="single__items">\n' +
-                '                                        <div class="miniproduct" id="minicart">\n' +
-                '                                            \n' +
-                '                                        </div>\n' +
-                '                                    </div>\n' +
-                '                                    <div class="mini_action cart">\n' +
-                '                                        <a class="cart__btn" th:href="@{/cart}">View and edit cart</a>\n' +
-                '                                    </div>\n' +
-                '                                </div>\n' +
-                '                            </div>')
         },
         error: function () {
             alert("Error mini carts")
@@ -156,7 +129,37 @@ function addToCart(idBook, idWishlist) {
     });
 }
 
-function updateCart(idCart ,quantity) {
+function updateAllCarts() {
+
+    var cartIds = [];
+    $('.classIdCart').each(function (i) {
+        cartIds.push($(this).val());
+    });
+    var cartQuantities = [];
+    $('.classCartQuantity').each(function (i) {
+        cartQuantities.push($(this).val());
+    });
+    var cartThisId = [];
+
+    console.log(cartIds);
+    console.log(cartQuantities);
+
+    $.ajax({
+       type: "GET",
+       url: "/cart/get-carts",
+       data: "JSON",
+       success: function (carts) {
+           carts.forEach(function (cart) {
+               cartThisId.push(cart.id);
+               console.log(cartThisId);
+           });
+
+           getCarts();
+       }
+    });
+}
+
+function updateCart(idCart, quantity) {
     $.ajax({
         type: "POST",
         url: "/cart/update-cart",
@@ -167,4 +170,22 @@ function updateCart(idCart ,quantity) {
             alert('Internal error!');
         }
     });
+}
+
+function showContent(tableID) {
+    var tbl = document.getElementById(tableID);
+    var rCount = document.getElementById(table).rows.length;
+    try {
+        alert(tbl.rows[rCount-1].cells[0].children[0].value);
+
+    } catch (e) {
+        alert(e);
+    }
+
+}
+
+
+function myFunction(table) {
+    var x = document.getElementById(table).rows.length;
+    console.log("Found " + x + " tr elements in the table.");
 }
